62907ec21bc7dab3595f6c7ec1e646e8091df7da
Diksha <diksha.shaw.7189@gmail.com>
2023-01-06 15:13:46 +0530

    added hungry prompt

--- hungry.java
+++ hungry.java
@@ -1,6 +1,6 @@
 public class hungry {
     public static void main(String[] args) {
-System.out.println("i am hungry");        
+System.out.println("i am hungry and i know it");        
     }
     
 }
