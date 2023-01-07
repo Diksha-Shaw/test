0fbb735262cfd3d8a50a3efa68ac2448132b77cf
Diksha <diksha.shaw.7189@gmail.com>
2023-01-07 16:24:23 +0530

    a change

--- hungry.java
+++ hungry.java
@@ -1,6 +1,16 @@
+import javax.lang.model.util.ElementScanner14;
+
 public class hungry {
     public static void main(String[] args) {
-System.out.println("i am hungry and i know it");        
+        int i=0;
+        if(i==0)
+        {
+            System.out.println("i am hungry"); 
+        }
+        else
+        {
+            System.out.println("i am hungry and i know it");
+        }        
     }
     
 }
