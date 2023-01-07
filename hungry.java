ab79dd994b9d6b9bccbc19c4f196094613eb9494
Diksha <diksha.shaw.7189@gmail.com>
2023-01-07 18:52:32 +0530

    remove blah blah

--- hungry.java
+++ hungry.java
@@ -3,7 +3,7 @@ public class hungry {
         int i=0;
         if(i==0)
         {
-            System.out.println("i am hungry blah blah"); 
+            System.out.println("i am hungry "); 
         }
         else
         {
